e12c5035f042cf7ad3ff4b417c0d80bb2aceb5b9
Varshan <varshanrio05@gmail.com>
2026-04-15 07:49:53 +0530

    fix: handle missing user case during passport deserialization

--- server/auth.ts
+++ server/auth.ts
@@ -76,6 +76,9 @@ export function setupAuth(app: Express) {
   passport.deserializeUser(async (id: number, done) => {
     try {
       const user = await storage.getUser(id);
+      if (!user) {
+        return done(null, false);
+      }
       done(null, user);
     } catch (err) {
       done(err);
